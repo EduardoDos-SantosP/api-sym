0d81e626b69754d48641b87add50b4a55bc28f61
EduardoDos-SantosP <66569234+EduardoDos-SantosP@users.noreply.github.com>
2022-08-13 18:52:14 -0300

    Corrigindo bug de migration e realizando o migrate de Usuario

--- migrations/Version20220813214409.php
+++ migrations/Version20220813214409.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220704015757 extends AbstractMigration
+final class Version20220813214409 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220704015757 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE contabil (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(127) NOT NULL, descricao VARCHAR(511) DEFAULT NULL, valor DOUBLE PRECISION NOT NULL, data DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(127) NOT NULL, login VARCHAR(127) NOT NULL, senha VARCHAR(127) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE contabil');
+        $this->addSql('DROP TABLE usuario');
     }
 }
